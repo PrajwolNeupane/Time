95898227bca25ae0426100f0449fbdefc4fd5ad5
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-24 20:17:50 +0545

    Work Now

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-import express from 'express';
-const app = express();
+var element = document.getElementById("time");
+var time = 1;
+// Specify the target date and time
+const targetDate = new Date('2023-02-04T22:05:45');  // Example: January 1, 2022, 12:00:00
 
-app.use(express.static('public'));
+function getTime(){
+    const currentDate = new Date();
 
-// Define routes
-app.get('/', (req, res) => {
-  res.sendFile("index.html");
-});
+// Calculate the time difference
+const duration = currentDate - targetDate;
 
-// Start the server
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running`);
-});
+// Convert the duration to the desired units (e.g., milliseconds, seconds, minutes)
+const milliseconds = Math.abs(duration);
+const seconds = Math.floor(milliseconds / 1000);
+const minutes = Math.floor(seconds / 60);
+const hours = Math.floor(minutes / 60);
+const days = Math.floor(hours / 24);
+const years = Math.floor(days / 365);
+const months = Math.floor(days / 30) - years * 12;
+const realDays = days -  months*30 - years*12*30;
+// const months = 
+// const months = days % 30;
+
+
+element.innerText = `${years}/${months}/${realDays} ${hours % 24}:${minutes % 60}:${seconds % 60}`;
+    
+}
+getTime();
+setInterval(()=>{
+    getTime();
+},1000);
