4cdb6d38e9339276d0d36e631c62cbe0d58c29eb
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-05-24 18:57:30 +0545

    Final

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(process.env.PORT || 3000,, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${port}`);
 });
