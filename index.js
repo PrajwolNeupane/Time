3d321ab2016297a1c062d7ffb501b9ba9468fb9d
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-09-18 10:22:05 +0545

    bug:fixed month bug

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 var element = document.getElementById("time");
 var time = 1;
+
 // Specify the target date and time
-const targetDate = new Date('2023-02-04T22:05:45');  // Example: January 1, 2022, 12:00:00
+const targetDate = new Date("2023-02-04T22:05:45");
+// Example: February 4, 2023, 22:05:45
 
-function getTime(){
-    const currentDate = new Date();
+function getTime() {
+  const currentDate = new Date();
 
-// Calculate the time difference
-const duration = currentDate - targetDate;
+  // Calculate the time difference in milliseconds
+  const duration = currentDate - targetDate;
 
-// Convert the duration to the desired units (e.g., milliseconds, seconds, minutes)
-const milliseconds = Math.abs(duration);
-const seconds = Math.floor(milliseconds / 1000);
-const minutes = Math.floor(seconds / 60);
-const hours = Math.floor(minutes / 60);
-const days = Math.floor(hours / 24);
-const years = Math.floor(days / 365);
-const months = Math.floor(days / 30) - years * 12;
-const realDays = days -  months*30 - years*12*30;
-// const months = 
-// const months = days % 30;
+  // Convert the duration to the desired units (e.g., milliseconds, seconds, minutes)
+  const seconds = Math.floor(duration / 1000) % 60;
+  const minutes = Math.floor(duration / 1000 / 60) % 60;
+  const hours = Math.floor(duration / 1000 / 60 / 60) % 24;
+  const days = Math.floor(duration / 1000 / 60 / 60 / 24) % 30;
+  const months = (Math.floor(duration / 1000 / 60 / 60 / 24 / 30) % 12) + 1;
+  const years = Math.floor(duration / 1000 / 60 / 60 / 24 / 365);
 
-
-element.innerText = `${years}/${months}/${realDays} ${hours % 24}:${minutes % 60}:${seconds % 60}`;
-    
+  element.innerText = `${years}/${months}/${days} ${hours}:${minutes}:${seconds}`;
 }
+
 getTime();
-setInterval(()=>{
-    getTime();
-},1000);
+setInterval(() => {
+  getTime();
+}, 1000);
